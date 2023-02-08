2eb32823a8d85f9df02642a315080aa0c08db206
boogawoogawooga <116218225+boogawoogawooga@users.noreply.github.com>
2023-02-08 18:55:59 -0500

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book2 extends Item{
+    // Book item that extends Item
+
+    Book2(String itemName, String author, double price, int stock){
+        super(itemName, author, price, stock);
+    }
+
+}
