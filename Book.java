ed874eb40985661bb301d32fef4181bce9738a28
boogawoogawooga <116218225+boogawoogawooga@users.noreply.github.com>
2023-02-08 19:03:55 -0500

    Delete Book.java

--- Book.java
+++
@@ -1,8 +0,0 @@
-public class Book extends Item{
-    // Book item that extends Item
-
-    Book(String itemName, String author, double price, int stock){
-        super(itemName, author, price, stock);
-    }
-
-}
